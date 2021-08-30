82b5d86f47d4240171d19617f335a6908985726f
Hari Limaye <hari.limaye@arm.com>
2021-08-30 17:43:12 +0100

    Fairly decent implementation, with thread checking sync every 10s

--- connectivity/FEATURE_RTP_MIDI/include/RTPMIDI.h
+++ connectivity/FEATURE_RTP_MIDI/include/RTPMIDI.h
@@ -22,13 +22,28 @@
 #include <cstring>
 #include "netsocket/UDPSocket.h"
 #include "netsocket/NetworkInterface.h"
+#include "rtos/Thread.h"
+#include "events/EventQueue.h"
+#include "platform/NonCopyable.h"
+#include "MIDIMessage.h"
+using namespace std::chrono_literals;
+
+/* TODO: Remove this workaround */
+#ifndef UNITTEST
 #include "LWIPStack.h"
+#if BYTE_ORDER == LITTLE_ENDIAN
+    #define htonll(x) ((((uint64_t)htonl(x)) << 32) + htonl((x) >> 32))
+#else
+    #define htonll(x) ((uint64_t)x)
+#endif /* BYTE_ORDER == LITTLE_ENDIAN */
+#endif
 
 enum apple_midi_defs : uint32_t {
     SIGNATURE        =  0xFFFFU,
     PROTOCOL_VERSION =  2U,
     VPXCC            =  0x80U,
-    MPAYLOAD         =  0x61U
+    MPAYLOAD         =  0x61U,
+    BJZP             =  0b00100000U
 };
 
 
@@ -38,6 +53,7 @@ enum rtpmidi_command : uint32_t {
     CK               =  0x434BU,
     SYNC0            =  0U,
     SYNC1            =  1U,
+    SYNC2            =  2U
 };
 
 /* TODO: Find non-overlapping error range */
@@ -49,13 +65,13 @@ enum rtpmidi_error_t : int32_t {
 };
 
 /* Command Header */
-typedef struct {
+typedef struct __attribute__((packed)) {
     uint16_t signature {SIGNATURE};
     uint16_t command;
 } command_header_t;
 
 /* Session Exchange Packet */
-typedef struct {
+typedef struct __attribute__((packed)) {
     command_header_t command_header;
     uint32_t protocol_version {PROTOCOL_VERSION};
     uint32_t initiator_token;
@@ -64,16 +80,16 @@ typedef struct {
 } exchange_packet_t;
 
 /* Synchronization Packet */
-typedef struct {
+typedef struct __attribute__((packed)) {
     command_header_t command_header;
     uint32_t sender_ssrc;
     uint8_t count;
-    uint8_t padding[3];
+    uint8_t padding[3] {0, 0, 0};
     uint64_t timestamp[3];
 } synchronization_packet_t;
 
 /* MIDI Packet Header */
-typedef struct {
+typedef struct __attribute__((packed)) {
     uint8_t vpxcc {VPXCC};
     uint8_t mpayload {MPAYLOAD};
     uint16_t sequence_number;
@@ -81,17 +97,59 @@ typedef struct {
     uint32_t sender_ssrc;
 } midi_packet_header_t;
 
-class RTPMIDI {
-public:
-    RTPMIDI(NetworkInterface *net, UDPSocket *socket);
+/* Constant from MIDIMessage.h */
+constexpr size_t MAX_MIDI_COMMAND_LEN = MAX_MIDI_MESSAGE_SIZE;
 
-    rtpmidi_error_t connect();
+/* Number of preliminary synchronization calls required - based on testing */
+constexpr size_t INITIAL_SYNC_COUNT = 6;
+constexpr int SOCKET_TIMEOUT = 100;
+constexpr std::chrono::seconds SYNC_FREQUENCY{10s};
+
+class RTPMIDI : private mbed::NonCopyable<RTPMIDI> {
+public:
+    RTPMIDI() = delete;
+
+    RTPMIDI(uint32_t ssrc,
+            std::string &&name,
+            uint16_t port = 5004,
+            bool connect_immediately = true,
+            NetworkInterface *net = NetworkInterface::get_default_instance(),
+            UDPSocket *control_socket = new UDPSocket,
+            UDPSocket *midi_socket = new UDPSocket);
+
+    ~RTPMIDI();
+
+
+    rtpmidi_error_t connect_and_sync(uint16_t control_port);
+    rtpmidi_error_t connect(uint16_t control_port);
+    void synchronise();
+    void write(MIDIMessage msg);
+    void send_midi_buffer();
 private:
+    uint32_t _ssrc;
+    std::string _name;
+    rtos::Thread _rtp_thread;
+    events::EventQueue _rtp_queue{32 * EVENTS_EVENT_SIZE};
+
+    static constexpr size_t CONTROL_SOCKET = 0;
+    static constexpr size_t MIDI_SOCKET = 1;
     NetworkInterface *_net;
-    UDPSocket *_socket;
+    UDPSocket *_sockets[2];
+    SocketAddress _peer_address;
+
+    static constexpr size_t MidiCommandHeaderPos = sizeof(midi_packet_header_t);
+    static constexpr size_t MidiHeaderLength = MidiCommandHeaderPos + 1; /* Command Header Byte */
+    static constexpr size_t MaxMidiPacketSize = MidiHeaderLength + MAX_MIDI_COMMAND_LEN;
+    static constexpr size_t StartOfMidiMessages = MidiHeaderLength;
+    uint8_t midi_buffer[MaxMidiPacketSize];
+    size_t midi_buffer_pos = StartOfMidiMessages;
+    size_t midi_buffer_size = 0;
+    uint16_t sequence_number = 0;
 
     bool connect_to_network();
-    bool exchange_handshake();
+    void exchange_handshake(uint16_t port, size_t SOCKET);
+    exchange_packet_t exchange_response(const exchange_packet_t &invitation);
+    uint64_t get_current_timestamp();
 };
 
 #endif // RTP_MIDI_H
--- connectivity/FEATURE_RTP_MIDI/source/RTPMIDI.cpp
+++ connectivity/FEATURE_RTP_MIDI/source/RTPMIDI.cpp
@@ -16,48 +16,109 @@
  */
 
 #include "RTPMIDI.h"
+#include "rtos/Kernel.h"
+#include "events/Event.h"
 #include <cstdio>
 
-RTPMIDI::RTPMIDI(NetworkInterface *net, UDPSocket *socket) : _net{net}, _socket{socket}
+RTPMIDI::RTPMIDI(uint32_t ssrc,
+                 std::string &&name,
+                 uint16_t port,
+                 bool connect_immediately,
+                 NetworkInterface *net,
+                 UDPSocket *control_socket,
+                 UDPSocket *midi_socket)
+    : _ssrc{ssrc}, _name{name}, _net{net}, _sockets{control_socket, midi_socket}
 {
+    if (connect_immediately) {
+        connect_and_sync(port);
+    }
+
 }
 
-rtpmidi_error_t RTPMIDI::connect()
+RTPMIDI::~RTPMIDI()
 {
+    if (_net) {
+        _net->disconnect();
+    }
+    if (_sockets[CONTROL_SOCKET]) {
+        delete _sockets[CONTROL_SOCKET];
+    }
+    if (_sockets[MIDI_SOCKET]) {
+        delete _sockets[MIDI_SOCKET];
+    }
+}
+
+rtpmidi_error_t RTPMIDI::connect_and_sync(uint16_t control_port)
+{
+    auto error = connect(control_port);
+    if (error == RTPMIDI_ERROR_OK) {
+
+        for (size_t i = 0; i < INITIAL_SYNC_COUNT; ++i) {
+            synchronise();
+        }
+
+        /* Configure timeout */
+        _sockets[MIDI_SOCKET]->set_timeout(SOCKET_TIMEOUT);
+
+        events::Event<void()> sync_event = _rtp_queue.event(this, &RTPMIDI::synchronise);
+        sync_event.period(SYNC_FREQUENCY);
+        sync_event.post();
+        _rtp_thread.start(callback(&_rtp_queue, &events::EventQueue::dispatch_forever));
+
+    }
+
+    return error;
+}
+
+rtpmidi_error_t RTPMIDI::connect(uint16_t control_port)
+{
+    printf("Try to connect\n");
     if(!connect_to_network()) {
         return RTPMIDI_ERROR_CONNECT;
     }
 
-    exchange_handshake();
+    printf("Start handshake\n");
+    exchange_handshake(control_port, CONTROL_SOCKET);
+    exchange_handshake(control_port + 1, MIDI_SOCKET);
 
     return RTPMIDI_ERROR_OK;
 }
 
-bool RTPMIDI::exchange_handshake()
+void RTPMIDI::exchange_handshake(uint16_t port, size_t SOCKET)
 {
-    SocketAddress address;
+    SocketAddress bind_address;
+    bind_address.set_port(port);
+    _sockets[SOCKET]->bind(bind_address);
+
     exchange_packet_t invitation_packet;
+    exchange_packet_t response_packet;
+/* DEBUG */
+    printf("Waiting to receive\n");
+/* DEBUG */
+    _sockets[SOCKET]->recvfrom(&_peer_address, &invitation_packet, sizeof(exchange_packet_t));
 
-    printf("Waiting to connect");
-    _socket->recvfrom(&address, &invitation_packet, sizeof(invitation_packet));
+    response_packet = exchange_response(invitation_packet);
 
-    exchange_packet_t response_packet = {
-        SIGNATURE,
-        htons(ACCEPT_INV),
-        htonl(PROTOCOL_VERSION),
-        invitation_packet.initiator_token,
-        htonl(0xdbffa3a1),
-        "NAME"
-    };
+/* DEBUG */
+    printf("Send response\n");
+/* DEBUG */
+    _sockets[SOCKET]->sendto(_peer_address, &response_packet, sizeof(exchange_packet_t));
+}
 
-    _socket->sendto(address, &response_packet, sizeof(invitation_packet));
-    _socket->recvfrom(&address, &invitation_packet, sizeof(invitation_packet));
-    _socket->sendto(address, &response_packet, sizeof(invitation_packet));
+exchange_packet_t RTPMIDI::exchange_response(const exchange_packet_t &invitation)
+{
+    exchange_packet_t response;
+    response.command_header.command = htons(ACCEPT_INV);
+    response.protocol_version = htonl(PROTOCOL_VERSION);
+    response.initiator_token = invitation.initiator_token;
+    response.sender_ssrc = htonl(_ssrc);
+    strcpy(response.name, _name.c_str());
+    return response;
 }
 
 bool RTPMIDI::connect_to_network()
 {
-    if(!_net || !_socket) {
+    if(!_net || !_sockets[CONTROL_SOCKET] || !_sockets[MIDI_SOCKET]) {
         return false;
     }
 
@@ -65,8 +126,17 @@ bool RTPMIDI::connect_to_network()
     if(error != NSAPI_ERROR_OK) {
         return false;
     }
+/* DEBUG */
+    SocketAddress sockAddr;
+    _net->get_ip_address(&sockAddr);
+    printf("IP address is: %s\n", sockAddr.get_ip_address() ? sockAddr.get_ip_address() : "No IP");
+/* DEBUG */
 
-    error = _socket->open(_net);
+    error = _sockets[CONTROL_SOCKET]->open(_net);
+    if(error != NSAPI_ERROR_OK) {
+        return false;
+    }
+    error = _sockets[MIDI_SOCKET]->open(_net);
     if(error != NSAPI_ERROR_OK) {
         return false;
     }
@@ -74,3 +144,71 @@ bool RTPMIDI::connect_to_network()
     return true;
 }
 
+void RTPMIDI::synchronise()
+{
+    SocketAddress sockAddr;
+    synchronization_packet_t sync_0;
+    synchronization_packet_t sync_1;
+    synchronization_packet_t sync_2;
+
+    _sockets[MIDI_SOCKET]->recvfrom(&sockAddr, &sync_0, sizeof(synchronization_packet_t));
+
+    sync_1.command_header.command = htons(CK);
+    sync_1.sender_ssrc = htonl(_ssrc);
+    sync_1.count = SYNC1;
+    sync_1.timestamp[SYNC0] = sync_0.timestamp[SYNC0];
+    sync_1.timestamp[SYNC1] = htonll(get_current_timestamp());
+
+    _sockets[MIDI_SOCKET]->sendto(sockAddr, &sync_1, sizeof(synchronization_packet_t));
+    _sockets[MIDI_SOCKET]->recvfrom(&sockAddr, &sync_2, sizeof(synchronization_packet_t));
+
+    /* Calculate time drift */
+}
+
+uint64_t RTPMIDI::get_current_timestamp()
+{
+    auto now = rtos::Kernel::Clock::now();
+    auto now_us = std::chrono::time_point_cast<std::chrono::microseconds>(now);
+    auto timestamp_us = now_us.time_since_epoch();
+    /* Return in units of 100us */
+    return timestamp_us.count() / 100;
+}
+
+void RTPMIDI::write(MIDIMessage msg)
+{
+    /* Check if we need to clear the buffer */
+    if (midi_buffer_size + msg.length > MAX_MIDI_COMMAND_LEN) {
+        send_midi_buffer();
+    }
+
+    /* Replace first byte (CN) with relative timestamp */
+    midi_buffer[midi_buffer_pos++] = 0x0;
+    for (int i = 1; i < msg.length; ++i) {
+        midi_buffer[midi_buffer_pos++] = msg.data[i];
+    }
+    midi_buffer_size += msg.length;
+}
+
+void RTPMIDI::send_midi_buffer()
+{
+    /* Set Midi Packet Header */
+    midi_packet_header_t *header_p = reinterpret_cast<midi_packet_header_t *>(midi_buffer);
+    header_p->vpxcc = VPXCC;
+    header_p->mpayload = MPAYLOAD;
+    header_p->sequence_number = htons(sequence_number++);
+
+    auto timestamp_64 = get_current_timestamp();
+    header_p->timestamp = htonl(static_cast<uint32_t>(timestamp_64));
+
+    header_p->sender_ssrc = htonl(_ssrc);
+
+    /* Set Command Header */
+    midi_buffer[MidiCommandHeaderPos] = (BJZP | midi_buffer_size);
+
+    /* Send the buffer */
+    _sockets[MIDI_SOCKET]->sendto(_peer_address, midi_buffer, MidiHeaderLength + midi_buffer_size);
+
+    /* Reset Buffer */
+    midi_buffer_pos = StartOfMidiMessages;
+    midi_buffer_size = 0;
+}
